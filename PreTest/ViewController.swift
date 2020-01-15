6dd3c97627ed28ed97f6a9c8d8a59e855bf7bb00
alfian0 <alfian.official.mail@gmail.com>
2020-01-15 19:59:10 +0700

    Initial Commit

---
+++ PreTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreTest
+//
+//  Created by alpiopio on 15/01/20.
+//  Copyright © 2020 alpiopio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
